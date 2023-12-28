8427259abfb426e3b38d610ed8e09a1af4b824b4
asrokknoivl <mqaisahm@gmail.com>
2023-12-28 05:07:41 +0100

    hello bitches heheheheh

---
+++ react-tryout/src/App.tsx
@@ -0,0 +1,9 @@
+import Message from "./Message"
+
+function App() {
+
+  // for imported React comps we could either close the tag or use a 'self concised' tag
+  return <div><Message /></div>
+}
+
+export default App;
---
+++ react-tryout/src/Message.tsx
@@ -0,0 +1,18 @@
+
+// PascalCasing
+// this typing style should be always followed when using function based components
+// maybe in React overall?
+// all in all, PascalCasing noted for React
+
+function Message() {
+    // it's happnining boys
+    // we finally meet the famous JSX
+    // the syntax belwow is JSX
+    // JSX stands for JavaScript XML
+    // so basically the h1 part is going to be compiled to js eventually
+    return <h1>HELLO BITCHES HEHEHEHEH</h1>;
+}
+
+// similarly to Angular, we gotta export the component to be able use it 
+
+export default Message; 
